a1687f910240d965d875b689e854986d1537132c
jacek010 <jmyjkowski@gmail.com>
2024-11-18 08:38:37 +0100

    add llm prepared datasets using two models: llama3.2 and gemma2

--- llm_generate_predictions.py
+++ llm_generate_predictions.py
@@ -4,18 +4,19 @@ import random as rd
 import json
 import time
 
-LLM_MODEL = 'llama3.2'
+LLM_MODEL = 'gemma2'
 
-PREDICT_ROWS = 10
+PREDICT_ROWS = 3000
 
-OUTPUT_SUFFIX = ""
+OUTPUT_SUFFIX = "new_explains"
 INPUT_FILE_NAME = '2018'
-OUTPUT_FILE_NAME = f'{INPUT_FILE_NAME}_llm_{LLM_MODEL.replace(".", "_")}_{OUTPUT_SUFFIX}'
+OUTPUT_FILE_NAME = f'{INPUT_FILE_NAME}_llm_{LLM_MODEL.replace(".", "_")}_{PREDICT_ROWS}_{OUTPUT_SUFFIX}'
 
-DATASETS_DIRECTORY = 'datasets'
+INPUT_DATASETS_DIRECTORY = 'datasets'
+OUTPUT_DATASETS_DIRECTORY = 'llm_prepared_datasets'
 
-INPUT_FILE = f'{DATASETS_DIRECTORY}/{INPUT_FILE_NAME}'
-OUTPUT_FILE = f'{DATASETS_DIRECTORY}/{OUTPUT_FILE_NAME}'
+INPUT_FILE = f'{INPUT_DATASETS_DIRECTORY}/{INPUT_FILE_NAME}'
+OUTPUT_FILE = f'{OUTPUT_DATASETS_DIRECTORY}/{OUTPUT_FILE_NAME}'
 
 
 
@@ -29,7 +30,7 @@ client = OpenAI(
 def predict(row:pd.Series)->str:
     context = f'''
     <context>
-    You are a helpful assistant, who is trying to help a user with predict fly delay.
+    You are a helpful assistant, who is trying to help a user to explain if him flight is delayed and why.
     As input you get the following information:
     <input>
     flight_info:
@@ -45,39 +46,20 @@ def predict(row:pd.Series)->str:
     </context>
     <task>
     - Based on this information, predict <delay_value> (number).
-    - Explain <reason> of delay (exacly one sentence).
+    - Explain <reason> of delay (maximum three sentences).
     - Do not predict based on wather.
-    - Predict only based on the information provided.
+    - DO not include delay value in <explain>
+    - Predict only based on the information provided and your knowledge.
     - Answer in JSON using <answer_template> and nothing more.
     - Do not return any other information.
     </task>
     <answer_template>
     {{
         "delay_minutes": "<delay_value>",
-        "delay_reason": "<reason>"
+        "delay_explain": "<explain>"
     }}
     </answer_template>
-    <example_answers>
-    - prediction:
-        delay_minutes: 10
-        delay_reason: "From this airport to that airport, the flight is always delayed."
-    - prediction:
-        delay_minutes: -20
-        delay_reason: "In this hour, the flight is always before the time."
-    - prediction:
-        delay_minutes: 0
-        delay_reason: "The flights of this carrier is always on time."
-    - prediction:
-        delay_minutes: 30
-        delay_reason: "The flights of this carrier is sometimes delayed."
-    - prediction:
-        delay_minutes: -10
-        delay_reason: "Flights from this airport are always before the time."
-    - prediction:
-        delay_minutes: 20
-        delay_reason: "Rush hours."
-    
-    </example_answers>
+
 '''
     flight_info = f'''
     Predict the delay of the following flight:
@@ -106,7 +88,7 @@ def modify_row(row_data:pd.Series)->pd.Series:
     while True:
         prediction = predict(row_data)
         
-        print(f"FL_DATA:\n {row_data[['FL_DATE', 'OP_CARRIER', 'OP_CARRIER_FL_NUM', 'ORIGIN', 'DEST', 'CRS_DEP_TIME', 'DISTANCE', 'CRS_ARR_TIME']]}\n")
+        print(f"FL_DATA:\n {row_data[['FL_DATE', 'OP_CARRIER', 'OP_CARRIER_FL_NUM', 'ORIGIN', 'DEST', 'CRS_DEP_TIME', 'DISTANCE', 'CRS_ARR_TIME', 'ARR_DELAY']]}\n")
         print(f"PREDICTION:\n {prediction}")
         
         prediction = prediction.replace('\n', '').replace('`','')
@@ -114,10 +96,10 @@ def modify_row(row_data:pd.Series)->pd.Series:
         
         try:
             json_prediction = json.loads(prediction)
-            print(f"DELAY: {json_prediction['delay_minutes']}, REASON: {json_prediction['delay_reason']}\n\n")
+            print(f"DELAY: {json_prediction['delay_minutes']}, REASON: {json_prediction['delay_explain']}\n\n")
             with pd.option_context('mode.chained_assignment', None):
                 row_data['llm_delay_minutes'] = json_prediction['delay_minutes']
-                row_data['llm_delay_reason'] = json_prediction['delay_reason']
+                row_data['llm_delay_explain'] = json_prediction['delay_explain']
             return row_data
         except:
             print(f"ERROR Running once again!\n")
@@ -125,9 +107,11 @@ def modify_row(row_data:pd.Series)->pd.Series:
 def main():
     df = pd.read_csv(f'{INPUT_FILE}.csv')
     df['llm_delay_minutes'] = None
-    df['llm_delay_reason'] = None
+    df['llm_delay_explain'] = None
     
     indexes = rd.sample(range(len(df)), PREDICT_ROWS)
+    
+    export_df = pd.DataFrame()
     
     start_time = time.time()
     
@@ -138,7 +122,10 @@ def main():
             row_data = df.iloc[i]
             
             modified_row = modify_row(row_data)
-            df.iloc[i] = modified_row
+            
+            # Update the row in the dataframe
+            # df.iloc[i] = modified_row
+            export_df = pd.concat([export_df, pd.DataFrame([modified_row])], ignore_index=True)
             
             f.write(f"{i}\n")
 
@@ -153,8 +140,8 @@ def main():
     'LLM_MODEL': '{LLM_MODEL}' 
 }}
         """)
-
-    df.to_csv(f'{OUTPUT_FILE}.csv', index=False)
+    export_df.to_csv(f'{OUTPUT_FILE}_export.csv', index=False)
+    # df.to_csv(f'{OUTPUT_FILE}.csv', index=False)
     
 
 if __name__ == '__main__':
